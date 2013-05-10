01dab8e274f95fa8b79fcd85bcb79975233d63ff
stefan <dev@2904.cc>
2013-05-10 19:54:10 +0200

    added oauth test

---
+++ oauth_test.py
@@ -0,0 +1,21 @@
+import tweepy
+import config
+
+# == OAuth Authentication ==
+#
+# This mode of authentication is the new preferred way
+# of authenticating with Twitter.
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# If the authentication was successful, you should
+# see the name of the account print out
+print api.me().name
+
+# If the application settings are set for "Read and Write" then
+# this line should tweet out the message to your account's
+# timeline. The "Read and Write" setting is on https://dev.twitter.com/apps
+api.update_status('Updating using OAuth authentication via Tweepy!')
