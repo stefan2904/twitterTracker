41705349d381e4c510a287daf1b956e37e90618c
stefan <dev@2904.cc>
2013-05-10 22:26:08 +0200

    unfollow tracker v0.2 - added resolution of (un)followed ids

--- unfollow.py
+++ unfollow.py
@@ -2,22 +2,22 @@ import tweepy
 from config import *
 from array import array
 
-# == Config==
+# == Config== ================================================================
 
 followerFile = "follower.dat"
 
-# == OAuth Authentication ==
+# == OAuth Authentication == =================================================
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-# == GET ids of all followers ==
+# == GET ids of all followers == =============================================
 
 newFollowerList = array('i')
 
-# == create array of current followers ==
+# == create array of current followers == ====================================
 
 for follower in tweepy.Cursor(api.followers_ids).items():
 	newFollowerList.append(follower)
@@ -25,7 +25,7 @@ for follower in tweepy.Cursor(api.followers_ids).items():
 
 print "Loaded %d follower-ids from Twitter" % len(newFollowerList)
 
-# == Load old follower-list ==
+# == Load old follower-list == ===============================================
 
 oldFollowerList = array('i')
 f = file(followerFile,"rb")
@@ -37,7 +37,7 @@ f.close()
 
 print "Read %d follower-ids from file" % len(oldFollowerList)
 
-# == Find (un)follows ==
+# == Find (un)follows == =====================================================
 
 unfollows = list(set(oldFollowerList) - set(newFollowerList))
 follows = list(set(newFollowerList) - set(oldFollowerList))
@@ -45,15 +45,15 @@ follows = list(set(newFollowerList) - set(oldFollowerList))
 print "there are", len(unfollows), "and", len(follows), "follows!"
 print ""
 
-# == identify (un)follows ==
-
-for unfollow in unfollows:
-	print unfollow, "unfollowed"
+# == identify (un)follows ====================================================
 
 for follow in follows:
-	print follow, "followed"
+	print follow, "\t", api.get_user(id=follow).screen_name, "\t followed"
 
-# == Backup current follower-list ==
+for unfollow in unfollows:
+	print unfollow, "\t", api.get_user(id=unfollow).screen_name, "\t unfollowed"
+
+# == Backup current follower-list == =========================================
 
 f = file(followerFile,"wb")
 newFollowerList.tofile(f)
