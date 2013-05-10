9e47d5acfdefc71798f72efd849b960eb4a55873
stefan <dev@2904.cc>
2013-05-10 23:30:01 +0200

    unfollow tracker v0.3 - implemented logfile and debug functions

--- unfollow.py
+++ unfollow.py
@@ -1,10 +1,52 @@
 import tweepy
 from config import *
 from array import array
+from datetime import datetime
+
+## TODO: Use more functions. ;-)
+## TODO: more elegant way to append content at the begining of the file (logBuffer)
 
 # == Config== ================================================================
 
 followerFile = "follower.dat"
+
+logFile = "unfollow.html"
+
+logLine = "<div class='%s'><a href='https://twitter.com/%s'><strong>%s</strong></a> %s at %s</div>"
+
+timestamp = "%d-%02d-%02d %02d:%02d" % (datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour, datetime.now().minute)
+
+DEBUG = False # Debug Output?
+
+# == Helper Methods == =========================================
+
+logBuffer = " " # logFile Cache 
+
+def writeFollow(name):
+	writeLog(name, "followed")
+
+def writeUnfollow(name):
+	writeLog(name, "unfollowed")
+
+def writeLog(name, action):
+	mydebug(name + " " + action)
+	global logBuffer
+	ll = (logLine % (action, name, name, action, timestamp)) + '\n'
+	logBuffer = ll + logBuffer
+
+def saveLog():
+	f = file(logFile, "r")
+	oldLog = f.read()
+	f.close()
+
+	f = file(logFile, "w")
+	f.write(logBuffer)
+	f.write(oldLog)
+	f.close()
+
+def mydebug(txt):
+	if(DEBUG == True):
+		print txt
 
 # == OAuth Authentication == =================================================
 
@@ -23,7 +65,7 @@ for follower in tweepy.Cursor(api.followers_ids).items():
 	newFollowerList.append(follower)
 	#print len(newFollowerList), "\t", follower
 
-print "Loaded %d follower-ids from Twitter" % len(newFollowerList)
+mydebug("Loaded %d follower-ids from Twitter" % len(newFollowerList))
 
 # == Load old follower-list == ===============================================
 
@@ -35,23 +77,25 @@ except EOFError:
     pass
 f.close()
 
-print "Read %d follower-ids from file" % len(oldFollowerList)
+mydebug("Read %d follower-ids from file" % len(oldFollowerList))
 
 # == Find (un)follows == =====================================================
 
 unfollows = list(set(oldFollowerList) - set(newFollowerList))
 follows = list(set(newFollowerList) - set(oldFollowerList))
 
-print "there are", len(unfollows), "and", len(follows), "follows!"
-print ""
+mydebug("there are " + str(len(unfollows)) + " and " + str(len(follows)) + " follows!")
+mydebug("-")
 
 # == identify (un)follows ====================================================
 
 for follow in follows:
-	print follow, "\t", api.get_user(id=follow).screen_name, "\t followed"
+	writeFollow(api.get_user(id=follow).screen_name)
 
 for unfollow in unfollows:
-	print unfollow, "\t", api.get_user(id=unfollow).screen_name, "\t unfollowed"
+	writeUnfollow(api.get_user(id=unfollow).screen_name)
+
+saveLog()
 
 # == Backup current follower-list == =========================================
 
