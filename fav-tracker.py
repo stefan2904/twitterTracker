841c37232cae6bb3ecad7bcb845eef5dd2fd0819
stefan <dev@2904.cc>
2016-08-09 05:55:00 +0200

    implemented simple retrieving of faved tweets

---
+++ fav-tracker.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, print_function
+import config as settings
+
+from tweepy import OAuthHandler
+from tweepy import API
+
+# == OAuth Authentication ==
+#
+# This mode of authentication is the new preferred way
+# of authenticating with Twitter.
+
+
+auth = OAuthHandler(settings.consumer_key, settings.consumer_secret)
+auth.secure = True
+auth.set_access_token(settings.access_token, settings.access_token_secret)
+
+api = API(auth)
+
+# print('Hi, %s!' % api.me().name)
+
+tweets = api.favorites()
+
+print('Loaded %d tweets ... ' % len(tweets))
+
+for tweet in tweets:
+    print('%s' % tweet.text.replace('\n', ' '))
+
+
